f22261bb3dc6d668c7db5a8f82b4afcf7fb28472
Wichai515 <wichai_pan@outlook.com>
2024-03-07 10:02:25 +0000

    change to docker server

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ctools_db',
         'USER': 'root',
-        'PASSWORD': 'wichai0515', #提交前删除
+        'PASSWORD': '', #提交前删除
         'HOST': '43.143.114.225',  # MySQL 服务器腾讯云
         'PORT': '3307',  # MySQL 端口为 3307
         # 'ENGINE': 'django.db.backends.mysql',
